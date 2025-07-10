f07b47403e903892a347f34ab385ab73800a0233
shanto096 <shantokumar096@gmail.com>
2025-07-10 19:23:08 +0300

    user data fetch

--- app/admin/user/page.js
+++ app/admin/user/page.js
@@ -1,4 +1,26 @@
+'use client'
+import { useEffect, useState } from "react";
+
 export default function UserPage() {
-    return <h1>Admin User Management</h1>;
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        setLoading(true);
+        const response = await fetch('/api/users');
+        const data = await response.json();
+        setUsers(data.data);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchUsers()
+  }, []);
+  console.log(users);
+    return <h1>Admin User Management{users?.length}</h1>;
   }
   
