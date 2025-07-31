0813bfe16e939c5653ba10b581a493907e039c2d
shanto096 <shantokumar096@gmail.com>
2025-07-31 08:45:31 +0300

    adding sidebar

--- app/components/Sidebar.jsx
+++ app/components/Sidebar.jsx
@@ -1,23 +1,32 @@
 'use client'
 import Link from "next/link";
 import React from "react";
+import { usePathname } from "next/navigation"; // ✅ import this
 import { useAuth } from "../context/AuthContext";
+
 const sidebarItems = [
   { name: "Dashboard", href: "/admin/dashboard" },
   { name: "User", href: "/admin/user" },
   { name: "Products", href: "/admin/products" },
   { name: "Website", href: "/" },
 ];
+
 const Sidebar = () => {
-    const {user} = useAuth()
+  const { user } = useAuth();
+  const pathname = usePathname(); // ✅ use this instead of location.pathname
 
   return (
-    <div>
+    <div className="fixed">
       <nav>
-        <ul className="list-none p-0">
+        <ul className="list-none ">
           {sidebarItems.map((item) => (
-            <li key={item.href} className="mb-4">
-              <Link href={item.href} className="no-underline text-gray-800">
+            <li
+              key={item.href}
+              className={`mb-4 rounded px-4 py-2 transition-colors  ${
+                pathname === item.href ? "bg-green-500 text-white " : "bg-white"
+              }`}
+            >
+              <Link href={item.href} className="no-underline text-inherit block">
                 {item.name}
               </Link>
             </li>
